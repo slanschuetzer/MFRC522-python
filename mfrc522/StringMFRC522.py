912251aec4ac127b41d4a3efdc7db5da5b2361d4
slanschuetzer <lani@gmx.at>
2020-09-26 21:16:05 +0200

    Adding authentication for additional sectors on writing

---
+++ mfrc522/StringMFRC522.py
@@ -0,0 +1,98 @@
+# Code by Simon Monk https://github.com/simonmonk/
+# Adaption by Stefan Lanschützer https://github.com/slanschuetzer/
+
+# Just needed a possiblity to store more data onto my mifare-classic-cards, than fits on one sector.
+# In fact i only need to store one big string (c-like terminated by 0x00)
+# This library seemed to be easily adaptable to my needs.
+
+from . import MFRC522
+import RPi.GPIO as GPIO
+  
+class StringMFRC522:
+
+  READER = None
+  
+  KEY = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
+  #BLOCK_ADDRS = [4, 5, 6, 8, 9, 10, 12, 13, 14, 16, 17, 18, 20, 21, 22, 24, 25, 26, 28, 29, 30, 32, 33, 34,
+  #               36, 37, 38, 40, 41, 42, 44, 45, 46, 48, 49, 50, 52, 53, 54, 56, 57, 58, 60, 61, 62]
+  BLOCK_ADDRS = [4, 5, 6, 8, 9, 10]
+  
+  def __init__(self):
+    self.READER = MFRC522()
+  
+  def read(self):
+      id, text = self.read_no_block()
+      while not id:
+          id, text = self.read_no_block()
+      return id, text
+
+  def read_id(self):
+    id = self.read_id_no_block()
+    while not id:
+      id = self.read_id_no_block()
+    return id
+
+  def read_id_no_block(self):
+      (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
+      if status != self.READER.MI_OK:
+          return None
+      (status, uid) = self.READER.MFRC522_Anticoll()
+      if status != self.READER.MI_OK:
+          return None
+      return self.uid_to_num(uid)
+  
+  def read_no_block(self):
+    (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
+    if status != self.READER.MI_OK:
+        return None, None
+    (status, uid) = self.READER.MFRC522_Anticoll()
+    if status != self.READER.MI_OK:
+        return None, None
+    id = self.uid_to_num(uid)
+    self.READER.MFRC522_SelectTag(uid)
+    status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+    data = []
+    text_read = ''
+    if status == self.READER.MI_OK:
+        for block_num in self.BLOCK_ADDRS:
+            block = self.READER.MFRC522_Read(block_num) 
+            if block:
+            		data += block
+        if data:
+             text_read = ''.join(chr(i) for i in data)
+    self.READER.MFRC522_StopCrypto1()
+    return id, text_read
+    
+  def write(self, text):
+      id, text_in = self.write_no_block(text)
+      while not id:
+          id, text_in = self.write_no_block(text)
+      return id, text_in
+
+  def write_no_block(self, text):
+      (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
+      if status != self.READER.MI_OK:
+          return None, None
+      (status, uid) = self.READER.MFRC522_Anticoll()
+      if status != self.READER.MI_OK:
+          return None, None
+      id = self.uid_to_num(uid)
+      self.READER.MFRC522_SelectTag(uid)
+      status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+      self.READER.MFRC522_Read(11)
+      if status == self.READER.MI_OK:
+          data = bytearray()
+          data.extend(bytearray(text.ljust(len(self.BLOCK_ADDRS) * 16,chr(0)).encode('ascii')))
+          i = 0
+          for block_num in self.BLOCK_ADDRS:
+            # TODO: here we should authenticate to the new sector on sector-change.
+            self.READER.MFRC522_Write(block_num, data[(i*16):(i+1)*16])
+            i += 1
+      self.READER.MFRC522_StopCrypto1()
+      return id, text[0:(len(self.BLOCK_ADDRS) * 16)]
+      
+  def uid_to_num(self, uid):
+      n = 0
+      for i in range(0, 5):
+          n = n * 256 + uid[i]
+      return n
